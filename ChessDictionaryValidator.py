2897523dfc01841b9dbb1230f43543a36c29ee24
harshithm-ait <harshithm.24.beai@acharya.ac.in>
2025-05-26 18:36:46 +0530

    Add files via upload

---
+++ ChessDictionaryValidator.py
@@ -0,0 +1,7 @@
+print("Harshith Nayaka M ")
+print("1AY24AI043 ")
+print(" M ")
+def valid_chess_board(board):
+    # Add validation logic here
+    return True
+print(valid_chess_board({'h1': 'wking'}))  # Example usage
