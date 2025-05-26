c4c01ce3783812094cbd0c852a45178c5aca75a4
harshithm-ait <harshithm.24.beai@acharya.ac.in>
2025-05-26 18:33:39 +0530

    Add files via upload

---
+++ CharacterPictureGrid.py
@@ -0,0 +1,12 @@
+print("Harshith Nayaka M ")
+print("1AY24AI043 ")
+print(" M ")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.']]
+for x in range(len(grid[0])):
+    for y in range(len(grid)):
+        print(grid[y][x], end='')
+    print()
+
+    
