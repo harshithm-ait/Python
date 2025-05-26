54332f06812bb2844099c15e0045926057f7d322
harshithm-ait <harshithm.24.beai@acharya.ac.in>
2025-05-26 18:41:07 +0530

    Add files via upload

---
+++ FantasyGameInventory.py
@@ -0,0 +1,8 @@
+print("Harshith Nayaka M ")
+print("1AY24AI043 ")
+print(" M ")
+print("Your Name, Your USN, Your Section")
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42}
+print("Inventory:")
+for item, count in inventory.items():
+    print(f"{count} {item}")
