d1168d7c479efb57403cd0615ffede992b36fdf8
harshithm-ait <harshithm.24.beai@acharya.ac.in>
2025-05-26 18:27:07 +0530

    Add files via upload

---
+++ CommaCode.py
@@ -0,0 +1,8 @@
+print("Harshith Nayaka M ")
+print("1AY24AI043 ")
+print(" M ")
+items = ['apples', 'bananas', 'tofu', 'cats']
+if len(items) == 0:
+    print("Empty list")
+else:
+    print(', '.join(items[:-1]) + ', and ' + items[-1])
