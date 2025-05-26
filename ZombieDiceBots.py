6376d8b1a855e328c6e92290b339139452c56b62
harshithm-ait <harshithm.24.beai@acharya.ac.in>
2025-05-26 18:52:50 +0530

    Add files via upload

---
+++ ZombieDiceBots.py
@@ -0,0 +1,7 @@
+print("Harshith Nayaka M ")
+print("1AY24AI043 ")
+print(" M ")
+# Simplified zombie dice bot
+import random
+dice = ['brain']*3 + ['shotgun']*2 + ['runner']*1
+print("Bot rolls:", ', '.join(random.sample(dice, 3)))
